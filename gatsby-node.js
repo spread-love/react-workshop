d42fedad90d047a42fe93aabafac779066e1575f
Juliano Rafael <julianorsfarias@msn.com>
2019-02-27 23:48:39 -0300

    :art: Sort AllLessons query and add previous and next

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,25 +3,31 @@ const path = require("path")
 exports.createPages = ({ graphql, actions }) => {
   const { createPage } = actions
   return graphql(`
-    {
-      allMdx {
+    query AllLessons {
+      allMdx(sort: { fields: [frontmatter___order], order: ASC }) {
         edges {
           node {
             id
             frontmatter {
               path
+              title
             }
           }
         }
       }
     }
   `).then(result => {
-    result.data.allMdx.edges.forEach(({ node }) => {
+    const lessons = result.data.allMdx.edges
+    lessons.forEach(({ node }, index) => {
+      const next = lessons[index + 1]
+      const previous = lessons[index - 1]
       createPage({
         path: node.frontmatter.path,
         component: path.resolve(`./src/templates/lesson.js`),
         context: {
           id: node.id,
+          next,
+          previous,
         },
       })
     })
