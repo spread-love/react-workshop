7a2280f12df6c30c29b65d1d645d8563a3f33992
Juliano Rafael <julianorsfarias@msn.com>
2019-02-27 23:49:33 -0300

    :art: Add Title and previous/next links to lessons

--- src/templates/lesson.js
+++ src/templates/lesson.js
@@ -1,12 +1,26 @@
 import React from "react"
-import { graphql } from "gatsby"
+import { graphql, Link } from "gatsby"
 import { MDXRenderer } from "gatsby-mdx"
 
 import Layout from "../components/layout"
 
-const Lesson = ({ data: { mdx } }) => (
+const Lesson = ({ data: { mdx }, pageContext: { previous, next } }) => (
   <Layout>
+    <h1>{mdx.frontmatter.title}</h1>
     <MDXRenderer>{mdx.code.body}</MDXRenderer>
+    <div style={{ display: "flex", marginTop: 16, marginBottom: 16 }}>
+      {previous && (
+        <Link to={previous.node.frontmatter.path}>
+          {previous.node.frontmatter.title}
+        </Link>
+      )}
+      <div style={{ flex: 1 }} />
+      {next && (
+        <Link to={next.node.frontmatter.path}>
+          {next.node.frontmatter.title}
+        </Link>
+      )}
+    </div>
   </Layout>
 )
 
@@ -15,6 +29,9 @@ export default Lesson
 export const pageQuery = graphql`
   query getLesson($id: String) {
     mdx(id: { eq: $id }) {
+      frontmatter {
+        title
+      }
       code {
         body
       }
